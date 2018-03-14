dae98a3a8499256bec02c3743726875034a9585a
Paul Xi <paulxi@gmail.com>
2018-03-14 10:10:31 -0700

    Initialize project

---
+++ src/main/java/com/leetcode/algorithm/easy/twosum/Solution.java
@@ -0,0 +1,7 @@
+package com.leetcode.algorithm.easy.twosum;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return new int[]{0 , 1};
+    }
+}
---
+++ src/test/java/com/leetcode/algorithm/easy/twosum/SolutionTest.java
@@ -0,0 +1,12 @@
+package com.leetcode.algorithm.easy.twosum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class SolutionTest {
+    @Test public void testSomeLibraryMethod() {
+        Solution solution = new Solution();
+        assertArrayEquals(new int[]{0, 1}, solution.twoSum(new int[] {0, 1}, 1));
+    }
+}
