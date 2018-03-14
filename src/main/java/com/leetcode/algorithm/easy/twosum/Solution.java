03dc2fe1823d864688df9c90fc9be02e77541225
Paul Xi <paulxi@gmail.com>
2018-03-14 15:50:54 -0700

    Introduce checkstyle

--- src/main/java/com/leetcode/algorithm/easy/twosum/Solution.java
+++ src/main/java/com/leetcode/algorithm/easy/twosum/Solution.java
@@ -1,7 +1,7 @@
 package com.leetcode.algorithm.easy.twosum;
 
 public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        return new int[]{0 , 1};
-    }
+  public int[] twoSum(int[] nums, int target) {
+    return new int[]{0, 1};
+  }
 }
--- src/test/java/com/leetcode/algorithm/easy/twosum/SolutionTest.java
+++ src/test/java/com/leetcode/algorithm/easy/twosum/SolutionTest.java
@@ -1,12 +1,13 @@
 package com.leetcode.algorithm.easy.twosum;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class SolutionTest {
-    @Test public void testSomeLibraryMethod() {
-        Solution solution = new Solution();
-        assertArrayEquals(new int[]{0, 1}, solution.twoSum(new int[] {0, 1}, 1));
-    }
+  @Test
+  public void testTwoSum() {
+    Solution solution = new Solution();
+    assertArrayEquals(new int[]{0, 1}, solution.twoSum(new int[]{0, 1}, 1));
+  }
 }
