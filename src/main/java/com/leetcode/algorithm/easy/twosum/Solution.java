0a661b390ee0625ef7b12580eb87e36e206d35ef
Paul Xi <paulxi@gmail.com>
2018-03-15 16:07:47 -0700

    Submit TwoSum solution

--- src/main/java/com/leetcode/algorithm/easy/twosum/Solution.java
+++ src/main/java/com/leetcode/algorithm/easy/twosum/Solution.java
@@ -1,7 +1,28 @@
 package com.leetcode.algorithm.easy.twosum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+  /**
+   * Given an array of integers, return indices of the two numbers such that they add up to
+   * a specific target.
+   * @param nums an array of integers
+   * @param target a specific target
+   * @return
+   */
   public int[] twoSum(int[] nums, int target) {
-    return new int[]{0, 1};
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int index = 0; index < nums.length; index++) {
+      int item = nums[index];
+      int value = target - item;
+      Integer valueIndex = map.get(value);
+      if (valueIndex != null) {
+        return new int[]{valueIndex.intValue(), index};
+      }
+      map.put(item, index);
+    }
+    return new int[0];
   }
 }
--- src/test/java/com/leetcode/algorithm/easy/twosum/SolutionTest.java
+++ src/test/java/com/leetcode/algorithm/easy/twosum/SolutionTest.java
@@ -6,8 +6,20 @@ import org.junit.jupiter.api.Test;
 
 public class SolutionTest {
   @Test
-  public void testTwoSum() {
+  public void testTwoSumOk() {
     Solution solution = new Solution();
-    assertArrayEquals(new int[]{0, 1}, solution.twoSum(new int[]{0, 1}, 1));
+    assertArrayEquals(new int[]{0, 1}, solution.twoSum(new int[]{2, 7, 11, 15}, 9));
+  }
+
+  @Test
+  public void testTwoSumEmptyNums() {
+    Solution solution = new Solution();
+    assertArrayEquals(new int[0], solution.twoSum(new int[0], 9));
+  }
+
+  @Test
+  public void testTwoSumNotFound() {
+    Solution solution = new Solution();
+    assertArrayEquals(new int[0], solution.twoSum(new int[]{2, 11, 15}, 9));
   }
 }
