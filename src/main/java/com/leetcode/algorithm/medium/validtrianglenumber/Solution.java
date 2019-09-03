15a3c60c7545e181cfc8441a5633a15adf199bbb
Paul Xi <paulxi@gmail.com>
2019-09-03 11:16:35 -0700

    Submit Solutions

--- src/main/java/com/leetcode/algorithm/medium/validtrianglenumber/Solution.java
+++ src/main/java/com/leetcode/algorithm/medium/validtrianglenumber/Solution.java
@@ -7,13 +7,18 @@ class Solution {
     int count = 0;
     Arrays.sort(nums);
 
-    for (int i = 0; i < nums.length - 2; i++) {
-      int k = i + 2;
-      for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
-        while (k < nums.length && nums[i] + nums[j] > nums[k]) {
-          k++;
+    int n = nums.length;
+    for (int i = n - 1; i >= 2; i--) {
+      int l = 0;
+      int r = i - 1;
+
+      while (l < r) {
+        if (nums[l] + nums[r] > nums[i]) {
+          count += r - l;
+          r--;
+        } else {
+          l++;
         }
-        count += k - j - 1;
       }
     }
 
